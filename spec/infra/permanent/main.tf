8449c5fb78dc149c230b82608bec91cd11b24646
Toby Clemson <tobyclemson@gmail.com>
2021-05-18 14:52:30 +0100

    Fix Terraform 0.15 upgrade issues.

--- spec/infra/permanent/main.tf
+++ spec/infra/permanent/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_server_certificate" "service" {
   name = "wildcard-certificate-${var.component}"
-  private_key = file(var.service_certificate_private_key)
-  certificate_body = file(var.service_certificate_body)
+  private_key = filebase64(var.service_certificate_private_key)
+  certificate_body = filebase64(var.service_certificate_body)
 }
